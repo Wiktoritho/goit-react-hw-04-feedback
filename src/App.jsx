6519b40ce107882dfcf31b0b08113e4216b88221
Wiktor Gola <wiktor.gola.95@wp.pl>
2023-04-07 11:24:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,46 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { Section } from './components/Section';
 import { FeedbackOptions } from './components/FeedbackOptions';
 import { Statistics } from './components/Statistics';
-import css from "./App.module.css";
+import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const INITIAL_STATS = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+  total: 0,
+};
+
+export const App = () => {
+  const [stats, setStats] = useState(INITIAL_STATS);
+
+  const handleClick = kind => {
+    stats[kind] = stats[kind] + 1;
+    stats.total += 1;
+    setStats({ ...stats });
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((stats.good / stats.total) * 100);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    return total === 0 ? 0 : Math.round((good / total) * 100);
-  };
-
-  handleFeedback = option => {
-    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
-  };
-
-  render() {
-    const total = this.countTotalFeedback();
-    const positiveFeedback = this.countPositiveFeedbackPercentage();
-    return (
-      <div className={css.container}>
-        <Section title="Please leave Feedback">
-          <FeedbackOptions
-            onLeaveFeedback={this.handleFeedback}
-          ></FeedbackOptions>
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={total}
-            positivePercentage={positiveFeedback}
-            messsage="There is no feedback"
-          ></Statistics>
-        </Section>
-        </div>
-    );
-  }
-}
+  return (
+    <div className={css.container}>
+      <Section title="Please leave Feedback">
+        <FeedbackOptions
+          onLeaveFeedback={kind => handleClick(kind)}
+        ></FeedbackOptions>
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={stats.good}
+          neutral={stats.neutral}
+          bad={stats.bad}
+          total={stats.total}
+          positivePercentage={countPositiveFeedbackPercentage()}
+          messsage="There is no feedback"
+        ></Statistics>
+      </Section>
+    </div>
+  );
+};
